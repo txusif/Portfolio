cb5aaed21d831503b715d675c8a0ec386e4434dd
Txusif <txusif@gmail.com>
2024-09-26 23:52:14 +0530

    updated Skills.tsx

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import Image from "next/image";
+import { useTheme } from "next-themes";
 
 import {
   Tooltip,
@@ -13,61 +16,79 @@ import javascript from "@/public/skills/javascript.svg";
 import typescript from "@/public/skills/typescript.svg";
 import nodejs from "@/public/skills/nodejs.svg";
 import react from "@/public/skills/react.svg";
-import nextjs from "@/public/skills/nextjs.svg";
 import redux from "@/public/skills/redux.svg";
 import tailwindcss from "@/public/skills/tailwind-css.svg";
 import figma from "@/public/skills/figma.svg";
-import github from "@/public/skills/github.svg";
 import postman from "@/public/skills/postman.svg";
 import mongodb from "@/public/skills/mongodb.svg";
+import shadcnui from "@/public/skills/shadcn-ui.png";
+import reactQuery from "@/public/skills/react-query.svg";
+
 import expressjs from "@/public/skills/express-js.svg";
-import shadcnui from "@/public/skills/shadcn-ui.svg";
-// import { SiShadcnui } from "react-icons/si";
-// import expressDark from "@/public/skills/express-dark.svg";
+import expressjsDark from "@/public/skills/express-js-dark.svg";
+import nextjs from "@/public/skills/next-js.svg";
+import nextjsDark from "@/public/skills/next-js-dark.svg";
+import github from "@/public/skills/github.svg";
+import githubDark from "@/public/skills/github-dark.svg";
 
 const skills = [
-  { name: "HTML", image: html5, icon: "" },
-  { name: "CSS", image: css, icon: "" },
-  { name: "JavaScript", image: javascript, icon: "" },
-  { name: "TypeScript", image: typescript, icon: "" },
-  { name: "Node.js", image: nodejs, icon: "" },
-  { name: "Express.js", image: expressjs, icon: "" },
-  { name: "React", image: react, icon: "" },
-  { name: "Next.js", image: nextjs, icon: "" },
-  { name: "React Query", image: react, icon: "" },
-  { name: "Redux", image: redux, icon: "" },
-  { name: "Tailwind CSS", image: tailwindcss, icon: "" },
-  { name: "Shadcn UI", image: shadcnui, icon: "<SiShadcnui />" },
-  { name: "Figma", image: figma, icon: "" },
-  { name: "MongoDB", image: mongodb, icon: "" },
-  // { name: "Supabase", image: <SiSupabase /> ,icon:""},
-  // { name: "PostgreSQL", image: <SiPostgresql /> ,icon:""},
-  // { name: "Git", image: <SiGit /> ,icon:""},
-  { name: "GitHub", image: github, icon: "" },
-  { name: "Postman", image: postman, icon: "" },
+  { name: "HTML", image: html5 },
+  { name: "CSS", image: css },
+  { name: "JavaScript", image: javascript },
+  { name: "TypeScript", image: typescript },
+  { name: "Node.js", image: nodejs },
+  {
+    name: "Express.js",
+    image: expressjs,
+    darkImage: expressjsDark,
+  },
+  { name: "React", image: react },
+  {
+    name: "Next.js",
+    image: nextjs,
+    darkImage: nextjsDark,
+  },
+  { name: "React Query", image: reactQuery },
+  { name: "Redux", image: redux },
+  { name: "Tailwind CSS", image: tailwindcss },
+  { name: "Shadcn UI", image: shadcnui },
+  { name: "Figma", image: figma },
+  { name: "MongoDB", image: mongodb },
+  {
+    name: "GitHub",
+    image: github,
+    darkImage: githubDark,
+  },
+  { name: "Postman", image: postman },
 ];
 
 const Skills = () => {
+  const { theme } = useTheme();
+
+  const isLight = theme === "light";
+
   return (
     <section id="skills" className="px-8 lg:px-0 space-y-8">
       <h1 className="text-center text-4xl font-medium">Skills</h1>
-      <ul className="grid grid-cols-8 gap-y-8">
+      <ul className="grid grid-cols-8 gap-y-8 gap-x-1 md:gap-x-0">
         {skills.map((skill, index) => (
           <li key={index} className="flex justify-center">
             <TooltipProvider delayDuration={100}>
               <Tooltip>
-                <TooltipTrigger className="group h-max w-max p-2 md:p-3 bg-secondary text-secondary-foreground rounded-xl flex justify-center items-center outline-none">
-                  <div className="text-2xl md:text-4xl transition-all duration-300">
-                    {/* {skill.icon} */}
-                    {skill.image ? (
-                      <Image
-                        src={skill.image}
-                        alt={skill.name}
-                        className="text-primary"
-                      />
-                    ) : (
-                      skill.icon
-                    )}
+                <TooltipTrigger className="group h-max w-max p-1 md:p-3 bg-secondary text-secondary-foreground rounded-xl flex justify-center items-center outline-none">
+                  <div className="transition-all duration-300">
+                    <Image
+                      src={
+                        skill.darkImage
+                          ? isLight
+                            ? skill.image
+                            : skill.darkImage
+                          : skill.image
+                      }
+                      alt={skill.name}
+                      width={48}
+                      height={48}
+                    />
                   </div>
                 </TooltipTrigger>
                 <TooltipContent className="font-medium text-sm">
