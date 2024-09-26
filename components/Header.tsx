d02e0f9c00084fd63ac9e896477f0b2225eb9060
Txusif <txusif@gmail.com>
2024-09-26 22:58:53 +0530

    fixed header

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import Nav from "./Nav";
 
 const Header = () => {
   return (
-    <div className="max-w-6xl mx-auto py-5 px-8 border-b">
+    <div className="max-w-6xl mx-auto py-5 px-8 border-b sticky top-0 z-50 backdrop-blur-3xl">
       <div className="flex justify-between items-center">
         <Logo />
         <Nav />
