c95345cdb12cac714292baff2d55d06cce7af448
txusif <txusif@gmail.com>
2024-12-15 23:22:40 +0530

    Updated: Resume link

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,45 +3,47 @@ import { buttonVariants } from "./ui/button";
 import Link from "next/link";
 
 const resumeLink =
-  "https://drive.google.com/file/d/1ocXKx_W1SB3gKGUhLMMzrJvMHcxv40Ot/view?usp=sharing";
+    "https://drive.google.com/file/d/1s1fe5Rj1yIrQlPA4lNq3TRXAzUb0J6cZ/view?usp=sharing";
 
 const Hero = () => {
-  return (
-    <section
-      id="home"
-      className="px-8 mt-20 flex flex-col md:flex-row items-center justify-around gap-12
+    return (
+        <section
+            id="home"
+            className="px-8 mt-20 flex flex-col md:flex-row items-center justify-around gap-12
       md:gap-0"
-    >
-      <div className="flex flex-col order-2 md:order-none">
-        <div className="flex flex-col items-center md:items-start ">
-          <h1 className="font-semibold text-4xl text-left">Toushief Ansari</h1>
-          <p>Full Stack Developer from Mumbai, India</p>
-          <p className="mt-4 text-lg italic text-center md:text-left">
-            Transforming Ideas into Engaging Digital Solutions
-          </p>
-        </div>
-        <div className="mt-6 text-center md:text-left">
-          <Link
-            href={resumeLink}
-            target="_blank"
-            className={buttonVariants({ variant: "default" })}
-          >
-            Download Resume
-          </Link>
-        </div>
-      </div>
-      <div className="order-1 md:order-none relative aspect-square h-[250px] md:h-[300px] lg:h-[350px] shadow-lg shadow-secondary rounded-full">
-        <Image
-          src={"/me.jpg"}
-          alt="txusif"
-          fill
-          className="object-contain rounded-full opacity-90"
-          quality={100}
-          priority
-        />
-      </div>
-    </section>
-  );
+        >
+            <div className="flex flex-col order-2 md:order-none">
+                <div className="flex flex-col items-center md:items-start ">
+                    <h1 className="font-semibold text-4xl text-left">
+                        Toushief Ansari
+                    </h1>
+                    <p>Full Stack Developer from Mumbai, India</p>
+                    <p className="mt-4 text-lg italic text-center md:text-left">
+                        Transforming Ideas into Engaging Digital Solutions
+                    </p>
+                </div>
+                <div className="mt-6 text-center md:text-left">
+                    <Link
+                        href={resumeLink}
+                        target="_blank"
+                        className={buttonVariants({ variant: "default" })}
+                    >
+                        Download Resume
+                    </Link>
+                </div>
+            </div>
+            <div className="order-1 md:order-none relative aspect-square h-[250px] md:h-[300px] lg:h-[350px] shadow-lg shadow-secondary rounded-full">
+                <Image
+                    src={"/me.jpg"}
+                    alt="txusif"
+                    fill
+                    className="object-contain rounded-full opacity-90"
+                    quality={100}
+                    priority
+                />
+            </div>
+        </section>
+    );
 };
 
 export default Hero;
